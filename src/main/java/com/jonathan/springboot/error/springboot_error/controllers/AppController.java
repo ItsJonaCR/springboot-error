0401a9bab7987f46cf1c6d681521d889b7ced3c2
ItsJonaCR <jonavasquez.cr@gmail.com>
2025-10-17 10:53:29 -0600

    New practice project. Adding configuration files and initial Spring Boot project structure

---
+++ src/main/java/com/jonathan/springboot/error/springboot_error/controllers/AppController.java
@@ -0,0 +1,14 @@
+package com.jonathan.springboot.error.springboot_error.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/app")
+    public String index() {
+        int value = 100 / 0;
+        return "Ok 200";
+    }
+}
