0e91915b18171269d1cd086db9ad65fbc680c894
ItsJonaCR <jonavasquez.cr@gmail.com>
2025-10-20 20:41:59 -0600

    Fix exception handling in AppController and add a handler for NumberFormatException in HandlerExceptionController

--- src/main/java/com/jonathan/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/jonathan/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,11 +1,14 @@
 package com.jonathan.springboot.error.springboot_error.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.jonathan.springboot.error.springboot_error.models.Error;
@@ -24,6 +27,17 @@ public class HandlerExceptionController {
         // return ResponseEntity.internalServerError().body(error);
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(NumberFormatException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("date", new Date());
+        error.put("error", "Invalid or incorrect number, no number type format.");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return error;
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
